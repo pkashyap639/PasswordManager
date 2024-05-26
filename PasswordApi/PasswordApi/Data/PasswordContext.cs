7f14f886dba2ce63134582e90cd50bb8f1355401
pkashyap639 <pkashyap_639@hotmail.com>
2024-05-26 02:55:27 -0400

    SetupDb

---
+++ PasswordApi/PasswordApi/Data/PasswordContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PasswordApi.Data
+{
+    public class PasswordContext:DbContext
+    {
+        public PasswordContext(DbContextOptions<PasswordContext> options):base(options)
+        {
+            
+        }
+    }
+}
