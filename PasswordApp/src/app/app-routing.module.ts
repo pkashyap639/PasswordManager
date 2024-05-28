d28309b6a76317c3ed38eef136bc62a7cbf1d883
pkashyap639 <pkashyap_639@hotmail.com>
2024-05-28 09:30:41 -0400

    Navbar

--- PasswordApp/src/app/app-routing.module.ts
+++ PasswordApp/src/app/app-routing.module.ts
@@ -4,12 +4,22 @@ import { SignInFormComponent } from './components/sign-in-form/sign-in-form.comp
 import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
+import { VaultComponent } from './components/dashboard/vault/vault.component';
+import { PasswordGeneratorComponent } from './components/dashboard/password-generator/password-generator.component';
+import { SettingsComponent } from './components/dashboard/settings/settings.component';
 
 const routes: Routes = [
    {path:'', component: SignInFormComponent},
    {path:'signin', component:SignInFormComponent},
    {path:'signup', component: SignUpFormComponent},
-   {path:'dashboard', component: DashboardComponent, canActivate:[authGuard]},
+   {path:'dashboard', component: DashboardComponent, canActivate:[authGuard],
+    children:[
+      {path:'', redirectTo:'vault', pathMatch:'full'},
+      {path:'vault', component: VaultComponent, canActivate:[authGuard]},
+      {path:'generator', component: PasswordGeneratorComponent, canActivate:[authGuard]},
+      {path:'settings', component: SettingsComponent, canActivate:[authGuard]},
+    ]
+   },
    {path: '**', redirectTo: 'signin', pathMatch: 'full'}
 
 
