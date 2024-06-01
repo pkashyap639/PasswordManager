b6a6bf70a94d3d7e0caa5ed7d11b7d246454c3ed
pkashyap639 <pkashyap_639@hotmail.com>
2024-06-01 13:33:35 -0400

    Password Generator

--- PasswordApp/src/app/components/dashboard/password-generator/password-generator.component.ts
+++ PasswordApp/src/app/components/dashboard/password-generator/password-generator.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PasswordService } from '../../../services/password.service';
+import { Clipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-password-generator',
   templateUrl: './password-generator.component.html',
   styleUrl: './password-generator.component.css'
 })
-export class PasswordGeneratorComponent {
+export class PasswordGeneratorComponent implements OnInit{
 
+  public newPassword:string = ""
+  constructor(private password:PasswordService, private clipboard:Clipboard){}
+  ngOnInit(): void {
+    
+  }
+
+  copyPasswordToClipboard(){
+    this.clipboard.copy(this.newPassword)
+  }
+  generateRandonPassword(){
+    this.password.generatePassword().subscribe({
+      next:(res)=>{
+        console.log(res.password);
+        this.newPassword = res.password
+        
+      },
+      error:(err)=>{
+        console.error(err);
+        
+      }
+    })
+  }
 }
